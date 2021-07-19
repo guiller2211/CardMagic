28432a7d121d2e8aae69bd0c879788b593bd617b
guiller1251 <69601710+guiller1251@users.noreply.github.com>
2021-07-19 00:57:04 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+<Inicio></Inicio>
+<Footer></Footer>
+   
+    
+  </div>
+</template>
+
+<script>
+import Inicio from './components/Inicio.vue'
+import Footer from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+   Inicio,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+
+
+</style>
